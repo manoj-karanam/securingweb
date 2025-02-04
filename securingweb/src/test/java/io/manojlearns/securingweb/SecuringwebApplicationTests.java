e707a0a5678f56ce44c4ba05753aaa9ae913bcea
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2025-02-04 13:30:11 -0500

    Auhentication with inmemory users

---
+++ securingweb/src/test/java/io/manojlearns/securingweb/SecuringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.manojlearns.securingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
